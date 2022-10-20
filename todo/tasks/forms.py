6e33a098da5066695c57792111d5890da5102646
Abdullah Fathy <71948732+Abdallah-Fathy@users.noreply.github.com>
2022-10-20 16:56:26 +0200

    Add files via upload

---
+++ todo/tasks/forms.py
@@ -0,0 +1,14 @@
+from socket import fromshare
+from django import forms
+from django.forms import ModelForm
+
+from .models import *
+
+
+class TaskForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Add New Task...'}))
+
+    class Meta:
+        model = Task
+        fields = '__all__'
